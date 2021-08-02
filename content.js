daa52bba279ac29a0822b5e1b58f6293c5685df8
GeekTek <raffinilorenzo@gmail.com>
2021-08-02 21:09:05 +0300

    WAweb frequesnt updates adaptations

--- content.js
+++ content.js
@@ -5,10 +5,13 @@ let otherUserPhone
 
 // WhatsApp Web class names
 // They change A LOT
-let textFieldClass = "_2_1wd copyable-text selectable-text"
+let textFieldClass = "_13NKt copyable-text selectable-text"
 let userIconClass = "_8hzr9 M0JmA i0jNr"
 let chatListClass = "_3uIPm WYyr1"
 let chatUserIconClass = "_1lPgH"
+
+// creating message class names
+let chatMessagesSectionClass = "y8WcF"
 
 // listener is triggered by the bundle.js (the background script)
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -208,10 +211,10 @@ function drawStatusCircle(scope) {
 
 function messageRecived(data) {
     let chatSection = document.getElementById("main")
-    let othersMessage = chatSection.getElementsByClassName("GDTQm message-in focusable-list-item")
+    let othersMessage = chatSection.getElementsByClassName("_2wUmf message-in focusable-list-item")
     if (othersMessage.length > 0) {
         let ll = othersMessage[othersMessage.length - 1]
-        let lll = ll.getElementsByClassName("_3-8er selectable-text copyable-text")[0]
+        let lll = ll.getElementsByClassName("i0jNr selectable-text copyable-text")[0]
         let lastOther = lll.getElementsByTagName("span")[0]
         if (lastOther.id != data.messageID && data.txt != "") {
             lastMessageID = data.messageID
@@ -238,121 +241,88 @@ function generateMessageID() {
 
 function createNewMessage(data) {
     let external = document.createElement('div')
-    external.tabIndex = "-1"
-    external.className = "GDTQm message-in focusable-list-item exterrrnal"
+external.tabIndex = "-1"
+external.className = "_2wUmf message-in focusable-list-item exterrrnal"
 
-    let uselessSpan = document.createElement('span')
-    external.appendChild(uselessSpan)
+let uselessSpan = document.createElement('span')
+external.appendChild(uselessSpan)
 
-    let div1 = document.createElement('div')
-    div1.className = "_24wtQ _2W7I-"
-    // div1.classList.add("_1-U5A")
+let div1 = document.createElement('div')
+div1.className = "Nm1g1 _22AX6"
 
-    let arrowSpan = document.createElement('span')
-    arrowSpan.classList.add("_1bUzr")
-    arrowSpan.setAttribute("data-testid", "tail-in")
-    arrowSpan.setAttribute("data-icon", "tail-in")
-    
-    // TODO message up-left arrow
-    /*
-    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg")
-    svg.setAttribute("viewBox", "0 0 8 13")
-    svg.setAttribute("width", "8")
-    svg.setAttribute("height", "13")
+let usernameSpan = document.createElement('span')
+usernameSpan.setAttribute("aria-label", "User:")
+div1.appendChild(usernameSpan)
 
-    let path1 = document.createElementNS('http://www.w3.org/2000/svg',"path")
+/* let arrowSpan = document.createElement('span')
+arrowSpan.classList.add("_1bUzr")
+arrowSpan.setAttribute("data-testid", "tail-in")
+arrowSpan.setAttribute("data-icon", "tail-in") */ 
+let div2 = document.createElement('div')
+div2.className = "_22Msk"
 
+let date = new Date()
+let h = date.getHours()
+if (h < 10) {
+    h = "0" + h
+}
+let mi = date.getMinutes()
+if (mi < 10) {
+    mi = "0" + mi
+}
+let g = date.getDay()
+let mo = date.getMonth()
+let y = date.getFullYear()
 
-        <span data-testid="tail-in" data-icon="tail-in" class="_1bUzr"
-      ><svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 8 13"
-        width="8"
-        height="13"
-      >
-        <path
-          opacity=".13"
-          fill="#0000000"
-          d="M1.533 3.568L8 12.193V1H2.812C1.042 1 .474 2.156 1.533 3.568z"
-        ></path>
-        <path
-          fill="currentColor"
-          d="M1.533 2.568L8 11.193V0H2.812C1.042 0 .474 1.156 1.533 2.568z"
-        ></path></svg
-    ></span>
-    */
-    
-    let div2 = document.createElement('div')
-    div2.className = "_3XpKm _20zqk"
-    let usernameSpan = document.createElement('span')
-    usernameSpan.setAttribute("aria-label", "User:")
-    div2.appendChild(usernameSpan)
+let div3 = document.createElement('div')
+div3.classList.add("copyable-text")
+div3.setAttribute("data-pre-plain-text", `[${h}:${mi}, ${g}/${mo}/${y}] User: `)
 
-    let div3 = document.createElement('div')
-    div3.classList.add("_1bR5a")
+let div4 = document.createElement('div')
+div4.classList.add("1Gy50")
 
-    let date = new Date()
-    let h = date.getHours()
-    if (h < 10) {
-        h = "0" + h
-    }
-    let mi = date.getMinutes()
-    if (mi < 10) {
-        mi = "0" + mi
-    }
-    let g = date.getDay()
-    let mo = date.getMonth()
-    let y = date.getFullYear()
+let spanText = document.createElement('span')
+spanText.className = "i0jNr selectable-text copyable-text"
+spanText.setAttribute("dir", "ltr")
 
-    let div4 = document.createElement('div')
-    div4.classList.add("copyable-text")
-    div4.setAttribute("data-pre-plain-text", `[${h}:${mi}, ${g}/${mo}/${y}] User: `)
-    
-    let div5 = document.createElement('div')
-    div5.classList.add("_3ExzF")
-    
-    let spanText = document.createElement('span')
-    spanText.className = "_3-8er selectable-text copyable-text"
-    spanText.setAttribute("dir", "ltr")
-    
-    let actualTextSpan = document.createElement('span')
-    let texxxt = document.createTextNode(data.txt)
-    actualTextSpan.appendChild(texxxt)
-    actualTextSpan.id = data.messageID
-    spanText.appendChild(actualTextSpan)
-    div5.appendChild(spanText)
-    
-    let afterTextSpan = document.createElement('span')
-    afterTextSpan.classList.add("_1Bd9o")
-    div5.appendChild(afterTextSpan)
-    div4.appendChild(div5)
-    div3.appendChild(div4)
-    
-    let div6 = document.createElement('div')
-    div6.classList.add("_2zWo9")
-    
-    let div7 = document.createElement('div')
-    div7.className = "UFTvj _89eWV"
-    
-    let timeSpan = document.createElement('span')
-    timeSpan.classList.add("_17Osw")
-    timeSpan.setAttribute("dir", "auto")
-    
-    let time = document.createTextNode(h + ":" + mi)
-    timeSpan.appendChild(time)
-    div7.appendChild(timeSpan)
-    div6.appendChild(div7)
-    div3.appendChild(div6)
-    div2.appendChild(div3)
+let actualTextSpan = document.createElement('span')
+let texxxt = document.createTextNode(data.txt)
+actualTextSpan.appendChild(texxxt)
+actualTextSpan.id = data.messageID
+spanText.appendChild(actualTextSpan)
 
-    let endSpan = document.createElement('span')
-    div2.appendChild(endSpan)
-    div1.appendChild(div2)
-    external.appendChild(div1)
+div4.appendChild(spanText)
 
-    let chatt = document.getElementsByClassName("_11liR")[0]
-    chatt.appendChild(external)
+let afterTextSpan = document.createElement('span')
+afterTextSpan.classList.add("_20bHr")
+div4.appendChild(afterTextSpan)
+div3.appendChild(div4)
+div2.appendChild(div3)
+
+let div5 = document.createElement('div')
+div5.classList.add("_1beEj")
+
+let div6 = document.createElement('div')
+div6.className = "_15K2I _3QDB6"
+
+let timeSpan = document.createElement('span')
+timeSpan.classList.add("kOrB_")
+timeSpan.setAttribute("dir", "auto")
+
+let time = document.createTextNode(h + ":" + mi)
+timeSpan.appendChild(time)
+div6.appendChild(timeSpan)
+div5.appendChild(div6)
+div3.appendChild(div5)
+div2.appendChild(div3)
+
+let endSpan = document.createElement('span')
+div2.appendChild(endSpan)
+div1.appendChild(div2)
+external.appendChild(div1)
+
+let chatt = document.getElementsByClassName(chatMessagesSectionClass)[0]
+chatt.appendChild(external)
 
     if (data.emoji == true) {
         let txt = external.getElementsByTagName("span")[3]
